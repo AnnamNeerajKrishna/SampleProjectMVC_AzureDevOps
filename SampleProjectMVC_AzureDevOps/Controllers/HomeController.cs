3fb3c2bd9817c197b371b1085abf7aa29d8e0d53
RDYTeddy <eastofcreating@gmail.com>
2023-03-22 17:09:18 +0530

    Add project files.

---
+++ SampleProjectMVC_AzureDevOps/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleProjectMVC_AzureDevOps.Models;
+using System.Diagnostics;
+
+namespace SampleProjectMVC_AzureDevOps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleProjectMVC_AzureDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleProjectMVC_AzureDevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
